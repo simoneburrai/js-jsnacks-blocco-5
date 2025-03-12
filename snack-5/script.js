96abdff966741e9c8c752a382298d2f587287563
simoneburrai <burrai.s98@gmail.com>
2025-03-12 13:41:22 +0100

    feat: folder imported, snacks completed

---
+++ snack-5/script.js
@@ -0,0 +1,7 @@
+const nums = [2, 8, 4, 7, 12, 87];
+
+// Crea un array a partire dall'array nums, che abbia solo i numeri pari
+// Risultato: [2, 8, 4, 12]
+
+const evenNums = nums.filter(num => num % 2 === 0);
+console.log(evenNums);
---
+++ snack-6/script.js
@@ -0,0 +1,19 @@
+const zucchine = [
+  { type: 'Napoletana', weight: 10, length: 4 },
+  { type: 'Trombetta', weight: 13, length: 16 },
+  { type: 'Napoletana', weight: 4, length: 23 },
+  { type: 'Trombetta', weight: 11, length: 6 },
+  { type: 'Napoletana', weight: 2, length: 17 },
+  { type: 'Romana', weight: 5, length: 10 },
+  { type: 'Romana', weight: 7, length: 9 },
+  { type: 'Trombetta', weight: 3, length: 8 },
+  { type: 'Calabrese', weight: 6, length: 27 },
+  { type: 'Calabrese', weight: 14, length: 4 },
+];
+
+// A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+const bigZucchine = zucchine.filter( element => element.length >= 15);
+console.log(bigZucchine)
+
+const littleZucchine = zucchine.filter( element => element.length < 15);
+console.log(littleZucchine);
